04b4c99f2bf63f6bf85ede893de26dc469786e59
Hordii <hordikk@MacBook-Air-Melchenko.local>
2026-03-14 18:05:27 +0100

    new update

--- 123.py
+++ 123.py
@@ -6,48 +6,65 @@ def mul(a, b):
     return a * b
 def div(a, b):
     return a / b
-
-history = []
-
-while True:
-    number_1 = int(input("Введите число: "))
-    number_2 = int(input("Введите число:"))
-    operace = int(input("1-сложение\n"
-                        "2-вычитание\n"
-                        "3-умножение\n"
-                        "4-деление:"))
-    if operace == 4 and number_2 == 0:
-        print("На ноль делить нельзя")
-        continue
-
-    try:
-        if operace == 1:
-            history.append(f"{number_1} + {number_2} = {add(number_1, number_2)} ")
-            print(add(number_1, number_2))
-        if operace == 2:
-            history.append(f"{number_1} - {number_2} = {sub(number_1, number_2)} ")
-            print(sub(number_1, number_2))
-        if operace == 3:
-            history.append(f"{number_1} * {number_2} = {mul(number_1, number_2)} ")
-            print(mul(number_1, number_2))
-        if operace == 4:
-            history.append(f"{number_1} / {number_2} = {div(number_1, number_2)} ")
-            print(div(number_1, number_2))
-
-        print(f" Вы провели такте операции:\n")
-        for operation in history:
-            print(f"{operation}")
+def get_number(prompt):
+    while True:
+        try:
+            return float(input(prompt))
+        except ValueError:
+            print("Введите коректное число!")
 
 
-        stop = input("Продолжить? (yes/no): ")
-        if stop == "no":
-           break
+def main():
+    history = []
+    while True:
+        number_1 = get_number("Введите число: ")
+        number_2 = get_number("Введите число:")
+        while True:
+            try:
+                operace = int(input("1-сложение\n"
+                                    "2-вычитание\n"
+                                    "3-умножение\n"
+                                    "4-деление:"))
+                if operace in [1, 2, 3, 4]:
+                    break
+                else:
+                    print("Введите коректное число")
+            except ValueError:
+                print("Введите коректное число")
 
 
-    except ZeroDivisionError:
-        print("На ноль делить нельзя")
-    except ValueError:
-        print("Вы ввели не число")
+        if operace == 4 and number_2 == 0:
+            print("На ноль делить нельзя")
+            continue
+
+        try:
+            if operace == 1:
+                history.append(f"{number_1} + {number_2} = {add(number_1, number_2)} ")
+                print(add(number_1, number_2))
+            if operace == 2:
+                history.append(f"{number_1} - {number_2} = {sub(number_1, number_2)} ")
+                print(sub(number_1, number_2))
+            if operace == 3:
+                history.append(f"{number_1} * {number_2} = {mul(number_1, number_2)} ")
+                print(mul(number_1, number_2))
+            if operace == 4:
+                history.append(f"{number_1} / {number_2} = {div(number_1, number_2)} ")
+                print(div(number_1, number_2))
+            print(f" Вы провели такие операции:\n")
+            for operation in history:
+                print(f"{operation}")
+
+
+            stop = input("Продолжить? (yes/no): ")
+            if stop == "no":
+               break
+
+
+        except ZeroDivisionError:
+            print("На ноль делить нельзя")
+        except ValueError:
+            print("Вы ввели не число")
+main()
 
 
 
