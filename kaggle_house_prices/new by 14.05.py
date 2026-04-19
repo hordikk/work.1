df42f630308291586f17f0881eba50b73e8acbaa
Hordii <hordikk@MacBook-Air-Melchenko.local>
2026-04-20 01:05:57 +0200

    update1.1(kaggle0.13)

--- kaggle_house_prices/new by 14.05.py
+++ kaggle_house_prices/new by 14.05.py
@@ -1,12 +1,13 @@
 import numpy as np
 import pandas as pd
 from lightgbm import LGBMRegressor
-from sklearn.model_selection import train_test_split
+from sklearn.model_selection import RandomizedSearchCV, GridSearchCV
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import mean_absolute_error
 from sklearn.model_selection import GroupKFold
 data_frame = pd.read_csv("train.csv")
 data_frame_test = pd.read_csv("test.csv")
+#Function
 def preprocesing(df):
     df = df.copy()
     only_str = df.select_dtypes(include="object").columns
@@ -25,6 +26,22 @@ def preprocesing(df):
 
     return df
 
+def tune(X, y, group):
+    gkf = GroupKFold(n_splits=3)
+
+    param_grid = {
+
+        'n_estimators': [500, 1000, 2000],
+        "learning_rate": [0.01, 0.03, 0.05],
+        "num_leaves": [32, 64, 128],
+    }
+    Search = RandomizedSearchCV(
+        LGBMRegressor(verbose=-1, random_state=42),
+        param_grid,
+        cv=gkf,
+    )
+    Search.fit(X, y, groups=group)
+    return Search.best_params_, Search.best_score_
 
 def train_model_cv(X, y, group, n_splits=5):
     gkf = GroupKFold(n_splits=n_splits)
@@ -35,38 +52,44 @@ def train_model_cv(X, y, group, n_splits=5):
         X_train, X_test = X.iloc[train_index], X.iloc[test_index]
         y_train, y_test = y.iloc[train_index], y.iloc[test_index]
         model = LGBMRegressor(
-            n_estimators=300,
+            n_estimators=500,
             random_state=42,
-            learning_rate=0.1,
-            predict_disable_shape_check=True
+            learning_rate=0.01,
+            predict_disable_shape_check=True,
+            num_leaves=128,
+            verbose=-1
         )
         model.fit(X_train, y_train)
         pred = model.predict(X_test)
         mae_list.append(mean_absolute_error(y_test, pred))
         mse_list.append(mean_squared_error(y_test, pred))
     final_model = LGBMRegressor(
-        n_estimators=300,
+        n_estimators=500,
         random_state=42,
-        learning_rate=0.1,
-        predict_disable_shape_check=True
+        learning_rate=0.01,
+        num_leaves=128,
+        predict_disable_shape_check=True,
+        verbose=-1
     )
     final_model.fit(X, y)
     return final_model, mae_list, mse_list
-
-X = preprocesing(data_frame.drop("Id", axis=1))
+#X, y, group
+X = preprocesing(data_frame.drop("SalePrice", axis=1))
 X_value = preprocesing(data_frame_test)
 X, X_value = X.align(X_value, join="left", axis=1, fill_value=0)
 y = np.log1p(data_frame["SalePrice"])
 groups = data_frame["Neighborhood"]
 
+#tune
+#best_params, best_score = tune(X, y, groups)
+#final check value
 final_model, mae_list, mse_list = train_model_cv(X, y, groups)
 y_pred_log = final_model.predict(X_value)
 y_pred = np.expm1(y_pred_log)
-print(mae_list)
+#result
 res = pd.DataFrame({
     "Id": data_frame_test["Id"],
     "SalePrice": y_pred,
 })
 res.to_csv("submission.csv", index=False)
-
-
+print(mae_list, mse_list)
